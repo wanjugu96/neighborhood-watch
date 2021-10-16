2ea2fa91037510e31fe0be9ae192c87a1ba80256
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-10-16 12:47:34 +0300

    sending email configurations

---
+++ InstagramApp/email.py
@@ -0,0 +1,16 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+def send_welcome_email(name, receiver):
+    #creating mesage subject and sender
+    subject="Welcome To InstagramClone"
+    sender='Testshelly29@gmail.com'
+
+    #passing context variables
+    text_context=render_to_string('email/newsemail.txt',{"name":name})
+    html_content=render_to_string('email/newsemail.html',{"name":name})
+
+    msg =EmailMultiAlternatives(subject,text_context,sender,[receiver])
+    msg.attach_alternative(html_content,'text/html')
+
+    msg.send()
