cc0b48d76b91e0ce020e2b05d21a594b3e66f19d
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-10-16 12:34:42 +0300

    adding nav bar to index file

--- InstagramApp/urls.py
+++ InstagramApp/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
+from . import  views
 
 urlpatterns = [
-    
+    url('^$',views.home,name='home'),
 ]
 
 if settings.DEBUG:
--- InstagramProject/settings.py
+++ InstagramProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'InstagramApp',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
